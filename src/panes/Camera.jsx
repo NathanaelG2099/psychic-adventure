1f71fd4ae0f865b3a92783d6ea48649fa7991673
Nathanael Garcia <nathanaelg2099@gmail.com>
2023-12-30 09:53:30 -0800

    MUI select -> TextField. Continue scale adjustment

--- src/panes/Camera.jsx
+++ src/panes/Camera.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import Box from "@mui/material/Box"
-import { Select } from '@mui/material'
+import { Select, TextField } from '@mui/material'
 import {MenuItem} from '@mui/material'
 
 const Camera = () => {
@@ -8,33 +8,62 @@ const Camera = () => {
   const [height, setHeight] = useState(innerHeight);
   const [width, setWidth] = useState(window.innerWidth);
 
+  const cameras = [
+    {
+      value: "Blank Video Feed",
+      label: "Clear"
+    },
+    {
+      value: "https://impeccabletablemanners.files.wordpress.com/2016/05/monkey-puppet-omg-shock-gif.gif",
+      label: 'Cam 1'
+    },
+    {
+      value: "https://art.ngfiles.com/images/1442000/1442802_amni3d_3d-among-us-gifs.gif?f1601359412",
+      label: 'Cam 2'
+    },
+    {
+      value: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/SmallFullColourGIF.gif',
+      label: 'Cam 3'
+    }
+  ]
   const handleChange = (event) => {
     setCam(event.target.value);
+    setHeight(innerHeight);
+    setWidth(innerWidth);
   }
 
   return (
     <>
+      <TextField fullWidth select style={{height: '55px'}} onChange={handleChange}>
+        {cameras.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      {/*
         <Select
         value={cam}
         label="Camera"
         onChange={handleChange}>
+          <MenuItem value={""}>Close</MenuItem>
           <MenuItem value={"https://impeccabletablemanners.files.wordpress.com/2016/05/monkey-puppet-omg-shock-gif.gif"}>Width {width}</MenuItem>
           <MenuItem value={"https://art.ngfiles.com/images/1442000/1442802_amni3d_3d-among-us-gifs.gif?f1601359412"}>Height {height}</MenuItem>
           <MenuItem value={"https://upload.wikimedia.org/wikipedia/commons/a/aa/SmallFullColourGIF.gif"}>Camera 3</MenuItem>
         </Select>
-        
+        */}
+
         <Box
         component="img"
         className='imgBox'
-        border={0}
-        borderTop={0}
-        borderLeft={0}
-        borderRight={0}
-        borderColor="white"
         sx={{
-            width: ({width} > {height})? "undefined":"",
-            height: ({width} > {height})? "100%":"undefined",
-            objectFit: 'contain'
+            width: '100%',
+            height: {innerHeight} - 50,
+            aspectRatio: 4/3,
+            display: 'flex',
+            flexGrow: 1,
+            boxSizing: 'border-box'
         }}
         alt="Video Feed."
         src={cam}/>
